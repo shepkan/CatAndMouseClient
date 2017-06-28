e7bee9ce9aa2208f730bb6e28fa4b4b4a856f1a6
KateVasovski <KateVasovski@gmail.com>
2017-06-28 15:03:39 +0300

    Test

--- src/EnteryPoint.java
+++ src/EnteryPoint.java
@@ -3,6 +3,6 @@
  */
 public class EnteryPoint {
     public static void main(String args[]){
-        System.out.println("YOUAREDURAK!");
+        System.out.println("Now we can talk here!");
     }
 }
